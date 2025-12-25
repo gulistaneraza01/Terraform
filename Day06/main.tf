17995c1a987804515d4349814d77a03da353ba22
Gulistane Raza <gulistaneraza01@gmail.com>
2025-12-25 23:51:01 +0530

    day06 file structure

---
+++ Day06/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "gulistaneraza-terraform-state"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
