cd0073241a5969d2fcf4696870252695d3c42b61
Gulistane Raza <gulistaneraza01@gmail.com>
2026-01-08 12:53:38 +0530

    read list map set object tuple

--- Day07/main.tf
+++ Day07/main.tf
@@ -1,7 +1,30 @@
 resource "aws_instance" "node_server" {
   ami                         = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type               = "t2.micro"
+  instance_type               = var.allow_vm_types[0]
   count                       = var.instance_count
   region                      = var.region
   associate_public_ip_address = var.associate_public_ip_address
+  tags                        = var.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = var.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.cidr_block[0]
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
--- Day07/variables.tf
+++ Day07/variables.tf
@@ -21,3 +21,32 @@ variable "associate_public_ip_address" {
   default     = false
   type        = bool
 }
+
+variable "cidr_block" {
+  description = "cidr block for vpc"
+  type        = list(string)
+  default     = ["10.0.0.0/8", "10.0.1.0/24", "172.168.0.0/16", "172.16.0.0/12"]
+}
+
+
+variable "allow_vm_types" {
+  description = "allow vm types"
+  type        = list(string)
+  default     = ["t2.micro", "t2.small", "t2.medium"]
+}
+
+variable "allowed_regions" {
+  description = "allowed regions"
+  type        = set(string)
+  default     = ["ap-south-1", "us-east-1", "us-west-1"]
+}
+
+
+variable "tags" {
+  description = "tags"
+  type        = map(string)
+  default = {
+    Name        = "allow_tls"
+    Environment = "Dev"
+  }
+}
