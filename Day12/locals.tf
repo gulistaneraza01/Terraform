5d902a045e5e4c456ea276576a0a9fc9407f87dc
Gulistane Raza <gulistaneraza01@gmail.com>
2026-01-20 22:07:31 +0530

    day13 data source

--- Day12/locals.tf
+++ Day12/locals.tf
@@ -5,7 +5,7 @@ locals {
   native_cost   = sum([for c in local.cost_numbers : c if c < 0])
   max_cost      = max(local.cost_numbers...)
   min_cost      = min(local.cost_numbers...)
-
+  
   current_timestamp          = timestamp()
   current_timestamp_formate1 = formatdate("DD-MM-YYYY", timestamp())
   current_timestamp_formate2 = formatdate("ddmmyyyy", timestamp())
