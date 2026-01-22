48d2b450809280cc6d50a73e61faff8bb8c02a3b
Gulistane Raza <gulistaneraza01@gmail.com>
2026-01-22 22:50:05 +0530

    day14 static hosting via tf

--- Day14/main.tf
+++ Day14/main.tf
@@ -19,3 +19,27 @@ resource "aws_cloudfront_origin_access_control" "static_oac" {
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
+
+resource "aws_s3_bucket_policy" "allow_cloud_front" {
+  bucket = aws_s3_bucket.staic_web_page.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
